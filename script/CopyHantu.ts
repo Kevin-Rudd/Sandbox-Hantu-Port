5c8ac420ebf2d223df966b4beb9a3d15a87d2fa8
KRudd <chuckytay@hotmail.com>
2023-07-22 14:06:45 +1000

    - Piepieonline script to copy chunk13 to chunk9 - Migrates chunk13 folder to chunk9

---
+++ script/CopyHantu.ts
@@ -0,0 +1,24 @@
+// Piepieonline's Hantu Freelancer deployment script v1
+
+import fs from "fs"
+import path from "path"
+
+export const analysis = (context, api) => Promise.resolve();
+export const beforeDeploy = (context, api) => Promise.resolve();
+
+export const afterDeploy: ModScript["afterDeploy"] = async function (context, api) {
+  const modID = context.deployInstruction.id;
+
+  api.logger.info('Copying Hantu Port to shared chunk for Freelancer access');
+  
+  fs.copyFileSync(context.config.runtimePath + '/chunk13.rpkg', context.config.runtimePath + '/chunk9patch200.rpkg');
+  fs.copyFileSync(context.config.runtimePath + '/chunk13patch1.rpkg', context.config.runtimePath + '/chunk9patch201.rpkg');
+  fs.copyFileSync(context.config.runtimePath + '/chunk13patch2.rpkg', context.config.runtimePath + '/chunk9patch202.rpkg');
+  fs.copyFileSync(context.config.runtimePath + '/chunk13patch3.rpkg', context.config.runtimePath + '/chunk9patch203.rpkg');
+  
+  return Promise.resolve();
+};
+
+export const cachingPolicy: ModScript["cachingPolicy"] = {
+  affected: []
+}
